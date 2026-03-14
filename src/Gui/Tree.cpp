84ff76d723e40e8a167b59c6b8d01d796b77c22a
Kacper Donat <kadet1090@gmail.com>
2026-03-14 19:11:16 +0100

    Gui: Apply "DocumentTree" component override to TreeWidget

--- src/Gui/Tree.cpp
+++ src/Gui/Tree.cpp
@@ -675,6 +675,8 @@ TreeWidget::TreeWidget(const char* name, QWidget* parent)
         _LastSelectedTreeWidget = this;
     }
 
+    setProperty("component", "DocumentTree");
+
     this->setDragEnabled(true);
     this->setAcceptDrops(true);
     this->setColumnCount(3);
